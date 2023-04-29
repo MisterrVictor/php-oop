1134bf70add63d5ba23f62c4df50018f3fc19fa9
MisterrVictor <85834329+MisterrVictor@users.noreply.github.com>
2023-04-29 18:48:06 +0300

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,22 @@
+<?php
+class Database
+{
+    // укажите свои собственные учетные данные для базы данных
+    private $host = "localhost";
+    private $db_name = "php_oop";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+    // получение соединения с базой данных
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch (PDOException $exception) {
+            echo "Ошибка соединения: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
